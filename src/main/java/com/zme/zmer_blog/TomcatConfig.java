c6e06093a3174e4cf826d8674fc3b40adc4a3bd7
dahuil <1402193626@qq.com>
2020-03-25 01:49:40 +0800

    修改归档中的时间倒序显示

--- src/main/java/com/zme/zmer_blog/TomcatConfig.java
+++ src/main/java/com/zme/zmer_blog/TomcatConfig.java
@@ -10,30 +10,30 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class TomcatConfig {
-    @Bean
-    public TomcatServletWebServerFactory serverContainer(){
-        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
-            @Override
-            protected void postProcessContext(Context context){
-                SecurityConstraint securityConstraint = new SecurityConstraint();
-                securityConstraint.setUserConstraint("CONFIDENTIAL");
-                SecurityCollection securityCollection = new SecurityCollection();
-                securityCollection.addPattern("/*");
-                securityConstraint.addCollection(securityCollection);
-                context.addConstraint(securityConstraint);
-            }
-        };
-        tomcat.addAdditionalTomcatConnectors(httpConnector());
-        return tomcat;
-    }
-
-    @Bean
-    public Connector httpConnector(){
-        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
-        connector.setScheme("http");
-        connector.setPort(80);
-        connector.setSecure(false);
-        connector.setRedirectPort(443);
-        return connector;
-    }
+//    @Bean
+//    public TomcatServletWebServerFactory serverContainer(){
+//        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory(){
+//            @Override
+//            protected void postProcessContext(Context context){
+//                SecurityConstraint securityConstraint = new SecurityConstraint();
+//                securityConstraint.setUserConstraint("CONFIDENTIAL");
+//                SecurityCollection securityCollection = new SecurityCollection();
+//                securityCollection.addPattern("/*");
+//                securityConstraint.addCollection(securityCollection);
+//                context.addConstraint(securityConstraint);
+//            }
+//        };
+//        tomcat.addAdditionalTomcatConnectors(httpConnector());
+//        return tomcat;
+//    }
+//
+//    @Bean
+//    public Connector httpConnector(){
+//        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+//        connector.setScheme("http");
+//        connector.setPort(80);
+//        connector.setSecure(false);
+//        connector.setRedirectPort(443);
+//        return connector;
+//    }
 }
